79bfbe72a465dc83961c9e08d7d3caf78748535f
Thomas Noel <thomas.c.noel@gmail.com>
2024-03-23 18:32:16 -0700

    Configure webpack and tailwind; Setup app skeleton; remove gitignore files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import _ from 'lodash';
+import $ from 'jQuery';
+
+import html from "./index.html";
 
 function component() {
   const element = document.createElement('div');
 
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'Mr.', 'Webpack'], ' ');
 
   return element;
 }
 
-document.body.appendChild(component());
+// document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
   mode: 'development',
+  entry: {
+    index: './src/index.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 }
