9685f78b856bc693e89f9de4d2ea1fb084b13a6c
Thomas Noel <thomas.c.noel@gmail.com>
2024-03-26 07:54:36 -0700

    A wee bit o' development on index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,66 @@ import $ from 'jQuery';
 
 import html from "./index.html";
 
-function component() {
-  const element = document.createElement('div');
+class Task {
+  constructor({
+    title, 
+    description, 
+    dueDate, 
+    priority, 
+    issuer, 
+    issuedTo, 
+    category, 
+    notes
+  } = {}) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.issuer = issuer;
+    this.issuedTo = issuedTo;
+    this.category = category;
+    this.notes = notes;
+    this.issueDate = new Date();
+    this.done = false;
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'Mr.', 'Webpack'], ' ');
+    // Category enumeration
+    this.categories = Object.freeze({
+      DEVELOPMENT: 0,
+      MEETING: 1,
+      MAINTENANCE: 2,
+      OVERHEAD: 3,
+      DEPLOYMENT: 4
+    });
+  }
 
-  return element;
+  markFinished() {}
+
+  editPriority(priority) {}
+
+  editNotes(updatedNotes) {}
+
+  editTitle(newTitle) {}
+
+  editDescription(newDescription) {}
+
+  editDueDate(newDate) {}
+
+  editIssuedTo(newIssuedTo) {}
+
+  editCategory(newCategory) {}
 }
 
-// document.body.appendChild(component());
+/** Projects contain a bunch of tasks **/
+class Project {}
+
+class TaskRenderer {}
+
+/** ProjectRenderers have a bunch of TaskRenderers **/
+class ProjectRenderer {}
+
+/** The main application logic; Tudu has a bunch of Project instances **/
+class Tudu {}
+
+/** Renders the main application logic; TuduRenderer has a bunch of ProjectRenderers **/
+class TuduRenderer {}
+
