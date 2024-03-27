35526654d68326b7c0577dda349551aeeb5e01d7
Thomas Noel <thomas.c.noel@gmail.com>
2024-03-27 06:29:41 -0700

    Rough draft Tasks and Projects

--- src/index.js
+++ src/index.js
@@ -35,33 +35,118 @@ class Task {
     });
   }
 
-  markFinished() {}
+  markFinished() {
+    this.done = true;
+  }
 
-  editPriority(priority) {}
+  editPriority(priority) {
+    this.priority = priority;
+  }
 
-  editNotes(updatedNotes) {}
+  editNotes(updatedNotes) {
+    this.notes = updatedNotes;
+  }
 
-  editTitle(newTitle) {}
+  editTitle(newTitle) {
+    this.title = newTitle;
+  }
 
-  editDescription(newDescription) {}
+  editDescription(newDescription) {
+    this.description = newDescription;
+  }
 
-  editDueDate(newDate) {}
+  editDueDate(newDate) {
+    this.dueDate = newDate;
+  }
 
-  editIssuedTo(newIssuedTo) {}
+  editIssuedTo(newIssuedTo) {
+    this.issuedTo = newIssuedTo; 
+  }
 
-  editCategory(newCategory) {}
+  editCategory(newCategory) {
+    valid = false;
+    this.categories.forEach((item) => {
+      if (newCategory === item) {
+        valid = true;
+      }
+    });
+
+    if (valid) {
+      this.category = newCategory;
+    }
+  }
 }
 
+
 /** Projects contain a bunch of tasks **/
-class Project {}
+class Project {
+  #tasks;
+  constructor({
+    title, 
+    description, 
+    issuedTo, 
+    issuer, 
+    dueDate, 
+    tasks = [], 
+    priority = 0
+  } = {}) {
+    this.#tasks = tasks;
+    this.priority = priority;
+    this.title = title;
+    this.description = description;
+    this.issuedTo = issuedTo;
+    this.issuer = issuer;
+    this.dueDate = dueDate;
+    this.issueDate = new Date();
+    this.done = false;
+  }
+
+  addTask(task) {
+    this.#tasks.append(task);
+  }
+
+  getOrderedTasks() {
+    return this.#tasks.toSorted((a, b) => a.priority - b.priority);
+  }
+
+  markFinished() {
+    this.done = true;
+  }
+
+  /**
+    * In case a project is marked complete too soon
+    */
+  resuscitate() {
+    this.done = false;
+  }
+
+  editPriority(newPriority) {
+    this.priority = newPriority;
+  }
+
+  deleteDeadTasks() {
+    deadIdxs = [];
+    this.#tasks.forEach((elem, idx) => {
+      if (elem.done) {
+        deadIdxs.append(idx);
+      }
+    });
+
+    // Want to delete higher indices first
+    orderedDeadIdxs = deadIdxs.reverse();
+    orderedDeadIdxs.forEach((idx) => {
+      this.#tasks.splice(idx, 1);
+    });
+  }
+}
+
+/** The main application logic; Tudu has a bunch of Project instances **/
+class Tudu {}
 
 class TaskRenderer {}
 
 /** ProjectRenderers have a bunch of TaskRenderers **/
 class ProjectRenderer {}
-
-/** The main application logic; Tudu has a bunch of Project instances **/
-class Tudu {}
 
 /** Renders the main application logic; TuduRenderer has a bunch of ProjectRenderers **/
 class TuduRenderer {}
